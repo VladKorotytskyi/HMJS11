f07e8cc647beda4eabe6d8899d2f7d93314af72f
DizzyYT <153006021+VladKorotytskyi@users.noreply.github.com>
2025-03-13 16:43:14 +0100

    ///

---
+++ main.js
@@ -0,0 +1,40 @@
+//1
+
+//Створити розмітку з кнопкою та текстовим полем.
+// За допомогою JavaScript отримати доступ до кнопки та текстового поля за їх ідентифікаторами
+// та змінити текст на кнопці на значення текстового поля.
+
+// const renameText = document.querySelector("#button");
+// const renameButton = document.querySelector("#text");
+// document.querySelector("#button").textContent = document.querySelector("#text").textContent;
+// console.log(renameText);
+// console.log(renameButton);
+
+//2
+
+//Створити розмітку з заголовком та зображенням.
+// За допомогою JavaScript отримати доступ до зображення та змінити значення атрибута "src" на шлях до іншого зображення.
+
+// const renameSrcImg = document.querySelector("img");
+// renameSrcImg.src =
+//   "https://media.4-paws.org/d/2/5/f/d25ff020556e4b5eae747c55576f3b50886c0b90/cut%20cat%20serhio%2002-1813x1811-720x719.jpg";
+// console.log(renameSrcImg);
+
+//3
+
+//Створити розмітку з посиланням та зображенням. За допомогою JavaScript отримати доступ до посилання
+// та змінити значення атрибута "href" на нову URL-адресу. Також отримати доступ до зображення та додати новий атрибут "alt" з описом зображення.
+
+// const changeUrl = document.querySelector("a");
+// changeUrl.href = "https://fonts.google.com/";
+
+// const changeAlt = document.querySelector("img");
+// changeAlt.alt = "dog";
+
+//4
+
+// Створити розмітку зі списком елементів. 
+// За допомогою JavaScript отримати доступ до першого елемента списку та змінити його вміст на новий текст.
+
+// const changeTextItem = document.querySelector('ul');
+// changeTextItem.firstElementChild.textContent = 'hello';
